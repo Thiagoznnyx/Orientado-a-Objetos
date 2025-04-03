c321bb56d204008c0e7f6db44d88ba8ac566f128
Thiagoznnyx <thiago_velho@estudante.sc.senai.br>
2025-04-02 21:36:54 -0300

    Update aula1.js

--- JavaScript.js/aula1.js
+++ JavaScript.js/aula1.js
@@ -2,19 +2,36 @@ const prompt = require('prompt-sync')();
 
 //Modelo atributos e funções para criação de um objeto
 class Cliente {
-nome;
-idade;
+
+//# Priva o dado
+#nome;
+#idade;
 cpf;
 agencia;
+
+getDescricao(){
+    return 'Nome' + this.#nome + ', idade' + this.#idade;
+}
+
+// Obtem nome
+ getNome(){
+    return  this.#nome;
+}
+ setNome(valor){
+    this.#nome = valor;
+}
+
 }
 
 // Objeto que possui as informações da classe de forma indivudal
 let cliente = new Cliente();
 
-//
-cliente.nome =  prompt('Digite o nome o cliente:');
-cliente.idade = prompt('Digite a idade:');
-cliente.cpf = prompt('Digite o seu CPF');
-cliente.agencia =prompt('Digite a agencia');
 
+cliente.setNome(prompt('Digite o nome o cliente:'));
+cliente.idade = prompt('Digite a idade:');
+cliente.cpf = prompt('Digite o seu CPF:');
+cliente.agencia =prompt('Digite a agencia:');
+
+console.log(cliente.getDescricao());
 console.log(cliente);
+console.log(cliente.getNome());
